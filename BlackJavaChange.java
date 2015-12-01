d73275257c2b964adabc3a694ade28a7aecdc075
oumaste <mohamed.oumaste@gmail.com>
2015-12-01 23:05:19 +0000

    Update BlackJavaChange.java

--- BlackJavaChange.java
+++ BlackJavaChange.java
@@ -13,7 +13,7 @@ public class BlackJack {
 	
 	}
 Julio => Question : Comment poser des questions pendant l'exam, il suffit de la poser ici et on va repondre tous !!! :)
-@ Thanks les gars
+oumaste @ Thanks les gars
 	public void getCartes(){
 		return cartes;
 		
