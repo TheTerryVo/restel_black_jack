16742b3563db061868045ae5317133b4f02e5681
jfataki <julio.christiano@gmail.com>
2015-12-01 22:57:03 +0100

    Create BlackJava.java

---
+++ BlackJava.java
@@ -0,0 +1,14 @@
+public class BlackJack {
+
+	public BlackJack() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	static int[] cartes = {1,2,3,4,5,6,7,8,9,10,10,10,10,
+			1,2,3,4,5,6,7,8,9,10,10,10,10,
+			1,2,3,4,5,6,7,8,9,10,10,10,10,
+			1,2,3,4,5,6,7,8,9,10,10,10,10,
+			1,1,1,1
+	};
+	
+	}
